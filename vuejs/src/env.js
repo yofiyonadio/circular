c5dfa7aa8fed86d7612466b2f4c60ba7351ac740
yofi yonadio <yofiyonadio@gmail.com>
2021-09-26 00:31:09 +0700

    Build Production

--- vuejs/src/env.js
+++ vuejs/src/env.js
@@ -1,6 +1,7 @@
 const SECRET = '$2a$12$ff.6epJcAXTsb28fNM.F1.DMeV9n/U8uWpsr.pGEmsnAY8y/hzZ5S'
 const APP_ORIGIN = window.location.origin
-const API_ORIGIN = 'http://localhost:3000'
+//const API_ORIGIN = 'http://localhost:3000'
+const API_ORIGIN = window.location.origin
 
 
 export {
